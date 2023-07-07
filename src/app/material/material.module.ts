fa758398759753ae8444b4186efe89ac871df4ed
= <sted96@gmail.com>
2023-07-07 13:38:11 +0200

    Update material.module.ts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
   declarations: [],
-  imports: [],
-  exports: [],
+  imports: [MatAutocompleteModule, MatFormFieldModule],
+  exports: [MatAutocompleteModule, MatFormFieldModule],
 })
 export class MaterialModule {}
