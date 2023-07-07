d8ad73a1105aa2563358224d0adcd3481ddb54d5
LauraSaporoso <laurasaporoso99@gmail.com>
2023-07-07 23:15:53 +0200

    css

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
-  imports: [MatTabsModule],
-  exports: [MatTabsModule],
+  imports: [MatTabsModule, MatInputModule, MatButtonModule],
+  exports: [MatTabsModule, MatInputModule, MatButtonModule],
 })
 export class MaterialModule {}
